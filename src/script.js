cfb7daa574e744fd897657b10900397b9a3d969b
Paulo Mariano <paulomariano2501@gmail.com>
2024-01-31 23:15:10 -0300

    📦 NEW: add moon and sun icons

--- src/script.js
+++ src/script.js
@@ -1,35 +1,29 @@
-var language = 'pt'; // Default language is Portuguese
+let languageBtn = document.getElementById("languageBtn");
+let language = "EN";
 
 function toggleLanguage() {
-    if (language === 'pt') {
-        // Change to English
-        document.getElementById('name').innerHTML = 'My name is Paulo Mariano';
-        document.getElementById('job').innerHTML = 'Web Developer';
-        document.getElementById('work').innerHTML = 'Work experience';
-        
-        var experienceItems = document.querySelectorAll('#experience > div');
-        experienceItems.forEach(item => {
-            item.querySelector('h1').innerHTML = 'Web Developer';
-            item.querySelector('h2').innerHTML = 'Company: 4Linux';
-            item.querySelector('h3').innerHTML = 'Period: 2019 - 2020';
-            item.querySelector('p').innerHTML = 'Development of web applications with PHP and Laravel';
-        });
-
-        language = 'en';
+    if (language === "EN") {
+        language = "PT";
     } else {
-        // Change to Portuguese
-        document.getElementById('name').innerHTML = 'Meu nome é Paulo Mariano';
-        document.getElementById('job').innerHTML = 'Desenvolvedor Web';
-        document.getElementById('work').innerHTML = 'Experiência profissional';
-        
-        var experienceItems = document.querySelectorAll('#experience > div');
-        experienceItems.forEach(item => {
-            item.querySelector('h1').innerHTML = 'Desenvolvedor Web';
-            item.querySelector('h2').innerHTML = 'Empresa: 4Linux';
-            item.querySelector('h3').innerHTML = 'Período: 2019 - 2020';
-            item.querySelector('p').innerHTML = 'Desenvolvimento de aplicações web com PHP e Laravel';
-        });
-
-        language = 'pt';
+        language = "EN";
     }
+    languageBtn.textContent = language;
+}
+
+let modeBtn = document.getElementById("modeBtn");
+let mode = "light";
+
+function toggleMode() {
+    const body = document.body;
+    const modeBtn = document.getElementById("modeBtn");
+    const isDark = body.classList.toggle("dark");
+    modeBtn.innerHTML = isDark ? getMoonSVG() : getSunSVG();
+}
+
+function getMoonSVG() {
+    return `<img src="icons/moon.svg" alt="moon" width="20px" />`;
+}
+
+function getSunSVG() {
+    return `<img src="icons/sun.svg" alt="sun" width="20px" />`;
 }
