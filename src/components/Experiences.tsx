408ef8b6b5fce22db10496e0964c8606e3e54730
Paulo Mariano <paulomariano2501@gmail.com>
2024-12-28 11:03:02 -0300

    refactor experiences

--- src/components/Experiences.tsx
+++ src/components/Experiences.tsx
@@ -7,8 +7,13 @@ const experiences = [
     jobTitle: "Front-End Developer",
     company: "LocX Real Estate",
     startDate: "03/2023",
-    description:
-      "At LocX, I joined as a frontend developer, where I primarily focused on maintaining the company's three internal projects using React and Redux. I also contributed to backend tasks using .NET, supporting the team when needed. My role involved ensuring the smooth functioning and updates of the applications, while occasionally assisting with backend development to enhance team productivity and project delivery.",
+    description: [
+      "Front-end development for internal projects, using React, JavaScript, and Redux.",
+      "Integration with internal APIs and services, ensuring communication between front-end and back-end.",
+      "Occasional support in back-end development, working with .NET and Entity Framework, as needed to assist the team.",
+      "Participation in planning and sprint review meetings, contributing to project evolution.",
+      "Assistance in project refactoring to improve performance and maintainability.",
+    ],
   },
   {
     id: 2,
@@ -16,8 +21,13 @@ const experiences = [
     company: "ILUMEO",
     startDate: "03/2023",
     endDate: "02/2024",
-    description:
-      "At Ilumeo, I worked as a full-stack developer, with a stronger focus on frontend development using React. On the backend, I utilized Node.js alongside TypeScript to build and maintain robust solutions. This role also introduced me to AWS cloud services, where I gained experience creating and managing buckets on the platform. It was a well-rounded experience that deepened my frontend expertise while expanding my knowledge of backend development and cloud infrastructure.",
+    description: [
+      "Front-end development for various company projects, using JavaScript, React, Context API, and TypeScript.",
+      "Back-end development with Node.js, TypeScript, Prisma, and PostgreSQL",
+      "Experience with AWS platform, configuring and managing infrastructure and services.",
+      "Developed a platform with around 500,000 registered users, ensuring scalability and efficiency.",
+      "Contributed to the evolution and maintenance of large-scale systems, focusing on performance and security.",
+    ],
   },
   {
     id: 3,
@@ -25,8 +35,12 @@ const experiences = [
     company: "Ketra Soluções Inteligentes",
     startDate: "09/2022",
     endDate: "01/2023",
-    description:
-      "At Ketra, I worked on a project for a local company, using .NET for the backend, React, and TypeScript for the frontend. I was fully responsible for the frontend development, crafting user interfaces and ensuring responsiveness and usability. While I focused on the frontend, I collaborated closely with a teammate who managed the backend development. This experience enhanced my ability to work in tandem with others, dividing responsibilities effectively to deliver a successful project.",
+    description: [
+      "Responsible for front-end development of a project for another company, using React, JavaScript, and TypeScript.",
+      "Collaborated with the team to create an efficient and responsive interface.",
+      "Ensured integration with external APIs and services, maintaining good system communication.",
+      "Occasional back-end changes to support the team as needed.",
+    ],
   },
   {
     id: 4,
@@ -34,8 +48,13 @@ const experiences = [
     company: "Infosis Consultoria e Sistemas",
     startDate: "06/2021",
     endDate: "09/2022",
-    description:
-      "At Infosis, I had my first experience as a full-stack developer, gaining hands-on knowledge in modern technologies such as ExtJs, .NET, React, and TypeScript. My responsibilities included developing and maintaining web applications, creating interactive and responsive user interfaces, and collaborating on backend APIs and systems. This role provided me with a strong foundation in both frontend and backend development, along with teamwork and problem-solving skills.",
+    description: [
+      "First professional contact with programming as an intern.",
+      "Learned and developed back-end using .NET and Entity Framework.",
+      "Front-end development with ExtJS and React.",
+      "Contributed to various projects, improving full-stack development skills.",
+      "First exposure to agile methodologies, working with Scrum.",
+    ],
   },
 ];
 
@@ -81,7 +100,13 @@ export default function Experiences() {
             <p className="text-gray-400 mb-4">
               {experience.startDate} - {experience.endDate || "Present"}
             </p>
-            <p className="text-gray-300">{experience.description}</p>
+            <ol className="text-gray-300 list-disc pl-6">
+              {experience.description.map((item) => (
+                <li className="mb-2" key={item}>
+                  {item}
+                </li>
+              ))}
+            </ol>
           </motion.div>
         ))}
       </motion.div>
