59b6cd7d8f2fafda69a6c3a1475b3e777b42c167
Paulo Mariano <paulomariano2501@gmail.com>
2025-02-04 23:01:33 -0300

    ssssss

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,14 +2,14 @@
 import { motion } from "framer-motion";
 
 export default function Projects() {
-  return (
-    <motion.section className="mb-16" initial="hidden" animate="visible">
-      <motion.h2 className="text-2xl font-bold text-orange-500 mb-8">
-        PROJECTS
-      </motion.h2>
+	return (
+		<motion.section className="mb-16" initial="hidden" animate="visible">
+			<motion.h2 className="text-2xl font-bold text-orange-500 mb-8">
+				PROJECTS
+			</motion.h2>
 
-      <motion.div className="flex justify-center space-x-8">
-        <motion.div className="bg-gray-800 p-4 rounded-lg">
+			<motion.div className="flex justify-center space-x-8">
+				{/* <motion.div className="bg-gray-800 p-4 rounded-lg">
           <a
             href="https://rena-fast.vercel.app"
             target="_blank"
@@ -23,21 +23,28 @@ export default function Projects() {
             <motion.h3 className="text-xl font-bold">RENAFAST</motion.h3>
             <motion.p className="text-gray-400">Photo renaming app</motion.p>
           </a>
-        </motion.div>
-
-        {/* <motion.div
-          className="bg-gray-800 p-4 rounded-lg"
-          variants={itemVariants}
-        >
-          <img
-            src="https://placehold.co/300x200"
-            alt="Project 2 thumbnail"
-            className="rounded mb-4"
-          />
-          <h3 className="text-xl font-bold">UNLOCK CSS MAGIC</h3>
-          <p className="text-gray-400">CSS TUTORIAL</p>
         </motion.div> */}
-      </motion.div>
-    </motion.section>
-  );
+				<motion.div className="bg-gray-800 p-4 rounded-lg max-w-xs mx-auto flex flex-col items-center">
+					<a
+						href="https://vercel-ui-sdk.vercel.app"
+						target="_blank"
+						rel="noreferrer"
+					>
+						<img
+							src="https://cdn2.iconfinder.com/data/icons/weather-flat-14/64/weather02-512.png"
+							alt="Project 2 thumbnail"
+							className="rounded mb-2 w-full h-auto max-h-[250px] max-w-xs bg-gray-900"
+						/>
+						<h3 className="text-xl font-bold text-white text-center">
+							VERCEL UI SDK WEATHER FORECAST{" "}
+						</h3>
+						<p className="text-gray-400 text-center">
+							Weather forecast application using artificial intelligence to see
+							the weather forecast of a city, using Vercel UI SDK
+						</p>
+					</a>
+				</motion.div>
+			</motion.div>
+		</motion.section>
+	);
 }
